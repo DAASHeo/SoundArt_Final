f5b012bb51d50a43e4074f5dc9af6573bb3ed63e
DAASHeo <hibstar719@naver.com>
2022-12-21 19:39:45 +0900

    터치 오류 수정

--- sketch.js
+++ sketch.js
@@ -2,11 +2,11 @@ let permission = false;
 var C4, D4, E4, F4, G4, A4, B4, C5, D5, E5;
 var pitches = [261.6256, 293.6648, 329.6276, 349.2282, 391.9954, 440, 493.8833, 523.2511, 587.3295, 659.2551];
 var val;
-var w;
+var wd;
 var margin = 50;
 
 function setup() {
-  createCanvas(680, 480);
+  createCanvas(1080, 480);
   
   //ios check start
   if(typeof DeviceMotionEvent.requestPermission == "function"){
@@ -78,13 +78,12 @@ function draw(){
   if(!permission) return;
   
   background(0);
-  w = 60;
+  wd = 60;
 
-  // 건반 모양 만들기
   for (var i = 0; i < pitches.length; i++) {
-    var x = i * w;
+    var x = i * wd;
     fill(255);
-    rect(x+margin, w, w-1, 200);
+    rect(x+margin, wd, wd-1, 200);
   }
 
   textSize(20);
@@ -100,67 +99,66 @@ function draw(){
   textSize(14);
   text("volume: "+val, 225+ margin, 25)
   
-  
-  //건반 터치 시 색 변화
+
   for(var i = 0; i < touches.length; i++){
-    if(w < touches[i].y && touches[i].y < 200 ){
-      if(margin< touches[i].x && touches[i].x < w-1+ margin){
+    if(wd < touches[i].y && touches[i].y <= wd +200 ){
+      if(margin< touches[i].x && touches[i].x < wd-1+ margin){
         fill(255, 0, 0);
-        rect(margin, w, w-1, 200);
+        rect(margin, wd, wd-1, 200);
         C4.freq(pitches[0]);
         C4.amp(val, 0.5);
       }
-      if(w+ margin < touches[i].x && touches[i].x < 20+(2*w)-1+ margin){
+      if(wd + margin < touches[i].x && touches[i].x < (wd*2)-1+ margin){
         fill(255, 100, 0);
-        rect(w+ margin, w, w-1, 200);
+        rect(wd+ margin, wd, wd-1, 200);
         D4.freq(pitches[1]);
         D4.amp(val, 0.5);
       }
-      if(2*w + margin < touches[i].x && touches[i].x < (3*w)-1 + margin){
+      if((wd*2) + margin < touches[i].x && touches[i].x < (wd*3)-1 + margin){
         fill(255, 255, 0);
-        rect(2*w+ margin, w, w-1, 200);
+        rect(2*wd+ margin, wd, wd-1, 200);
         E4.freq(pitches[2]);
         E4.amp(val, 0.5);
       }
-      if(3*w + margin< touches[i].x && touches[i].x < (4*w)-1+ margin){
+      if((wd*3) + margin< touches[i].x && touches[i].x < (wd*4)-1+ margin){
         fill(0, 255, 0);
-        rect(3*w+ margin, w, w-1, 200);
+        rect(3*wd+ margin, wd, wd-1, 200);
         F4.freq(pitches[3]);
         F4.amp(val, 0.5);
       }
-      if(4*w+ margin < touches[i].x && touches[i].x < (5*w)-1+ margin){
+      if((wd*4)+ margin < touches[i].x && touches[i].x < (wd*5)-1+ margin){
         fill(0, 0, 255);
-        rect(4*w+ margin, w, w-1, 200);
+        rect(4*wd+ margin, wd, wd-1, 200);
         G4.freq(pitches[4]);
         G4.amp(val, 0.5);
       }
-      if(5*w+ margin < touches[i].x && touches[i].x < (6*w)-1+ margin){
+      if((wd*5)+ margin < touches[i].x && touches[i].x < (wd*6)-1+ margin){
         fill(0, 5, 128);
-        rect(5*w+ margin, w, w-1, 200);
+        rect(5*wd+ margin, wd, wd-1, 200);
         A4.freq(pitches[5]);
         A4.amp(val, 0.5);
       }
-      if(6*w + margin< touches[i].x && touches[i].x < (7*w)-1+ margin){
+      if((wd*6) + margin< touches[i].x && touches[i].x < (wd*7)-1+ margin){
         fill(100, 0, 255);
-        rect(6*w+ margin, w, w-1, 200);
+        rect(6*wd+ margin, wd, wd-1, 200);
         B4.freq(pitches[6]);
         B4.amp(val, 0.5);
       }
-      if(7*w+ margin < touches[i].x && touches[i].x < (8*w)-1+ margin){
+      if((wd*7)+ margin < touches[i].x && touches[i].x < (wd*8)-1+ margin){
         fill(255, 0, 0);
-        rect(7*w+ margin, w, w-1, 200);
+        rect(7*wd+ margin, wd, wd-1, 200);
         C5.freq(pitches[7]);
         C5.amp(val, 0.5);
       }
-      if(8*w+ margin < touches[i].x && touches[i].x < (9*w)-1+ margin){
+      if((wd*8)+ margin < touches[i].x && touches[i].x < (wd*9)-1+ margin){
         fill(255, 50, 0);
-        rect(8*w+ margin, w, w-1, 200);
+        rect(8*wd+ margin, wd, wd-1, 200);
         D5.freq(pitches[8]);
         D5.amp(val, 0.5);
       }
-      if(9*w+ margin < touches[i].x && touches[i].x < (10*w)-1+ margin){
+      if((wd*9)+ margin < touches[i].x && touches[i].x < (wd*10)-1+ margin){
         fill(255, 255, 0);
-        rect(9*w+ margin, w, w-1, 200);
+        rect(9*wd+ margin, wd, wd-1, 200);
         E5.freq(pitches[9]);
         E5.amp(val, 0.5);
       }
@@ -185,35 +183,35 @@ function touchStarted(){
   getAudioContext().resume();
   
   for(var i = 0; i < touches.length; i++){
-    if(w < touches[i].y && touches[i].y < 200){
-      if(0 + margin < touches[i].x && touches[i].x < w-1+ margin){
+    if(wd < touches[i].y && touches[i].y <= wd + 200){
+      if(0 + margin < touches[i].x && touches[i].x < wd-1+ margin){
         C4.start();
       }
-      if(w+ margin < touches[i].x && touches[i].x < (2*w)-1+ margin){
+      if(wd+margin < touches[i].x && touches[i].x < (wd*2)-1+ margin){
         D4.start();
       }
-      if(2*w + margin< touches[i].x && touches[i].x < (3*w)-1+ margin){
+      if((wd*2) + margin< touches[i].x && touches[i].x < (wd*3)-1+ margin){
         E4.start();
       }
-      if(3*w + margin< touches[i].x && touches[i].x < (4*w)-1+ margin){
+      if((wd*3) + margin< touches[i].x && touches[i].x < (wd*4)-1+ margin){
         F4.start();
       }
-      if(4*w+ margin < touches[i].x && touches[i].x < (5*w)-1+ margin){
+      if((wd*4)+ margin < touches[i].x && touches[i].x < (wd*5)-1+ margin){
         G4.start();
       }
-      if(5*w+ margin < touches[i].x && touches[i].x < (6*w)-1+ margin){
+      if((wd*5)+ margin < touches[i].x && touches[i].x < (wd*6)-1+ margin){
         A4.start();
       }
-      if(6*w + margin< touches[i].x && touches[i].x < (7*w)-1+ margin){
+      if((wd*6) + margin< touches[i].x && touches[i].x < (wd*7)-1+ margin){
         B4.start();
       }
-      if(7*w+ margin < touches[i].x && touches[i].x < (8*w)-1+ margin){
+      if((wd*7)+ margin < touches[i].x && touches[i].x < (wd*8)-1+ margin){
         C5.start();
       }
-      if(8*w + margin< touches[i].x && touches[i].x < (9*w)-1+ margin){
+      if((wd*8) + margin< touches[i].x && touches[i].x < (wd*9)-1+ margin){
         D5.start();
       }
-      if(9*w+ margin < touches[i].x && touches[i].x < (10*w)-1+ margin){
+      if((wd+9)+ margin < touches[i].x && touches[i].x < (wd*10)-1+ margin){
         E5.start();
       }
     }
