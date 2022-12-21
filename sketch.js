35a3c6576e527d2f09b6658ba9d91611ed3c369f
DAASHeo <hibstar719@naver.com>
2022-12-21 19:18:33 +0900

    볼륨 0일때 멘트

--- sketch.js
+++ sketch.js
@@ -80,78 +80,87 @@ function draw(){
   background(0);
   w = 60;
 
+  // 건반 모양 만들기
   for (var i = 0; i < pitches.length; i++) {
     var x = i * w;
     fill(255);
-    rect(x+margin, 60, w-1, 200);
+    rect(x+margin, w, w-1, 200);
   }
 
   textSize(20);
   text('KEY BOARD', margin+50, 30);
   val = map(rotationX, 0, 100, 0, 1);
+  
+  if(val <= 0){
+    val = 0;
+    textSize(14);
+    text("기기를 움직여보세요!",225+ margin, 40)
+  }
+  
   textSize(14);
   text("volume: "+val, 225+ margin, 25)
   
   
+  //건반 터치 시 색 변화
   for(var i = 0; i < touches.length; i++){
-    if(60 < touches[i].y && touches[i].y < 200 ){
+    if(w < touches[i].y && touches[i].y < 200 ){
       if(margin< touches[i].x && touches[i].x < w-1+ margin){
         fill(255, 0, 0);
-        rect(margin, 60, w-1, 200);
+        rect(margin, w, w-1, 200);
         C4.freq(pitches[0]);
         C4.amp(val, 0.5);
       }
       if(w+ margin < touches[i].x && touches[i].x < 20+(2*w)-1+ margin){
         fill(255, 100, 0);
-        rect(w+ margin, 60, w-1, 200);
+        rect(w+ margin, w, w-1, 200);
         D4.freq(pitches[1]);
         D4.amp(val, 0.5);
       }
       if(2*w + margin < touches[i].x && touches[i].x < (3*w)-1 + margin){
         fill(255, 255, 0);
-        rect(2*w+ margin, 60, w-1, 200);
+        rect(2*w+ margin, w, w-1, 200);
         E4.freq(pitches[2]);
         E4.amp(val, 0.5);
       }
       if(3*w + margin< touches[i].x && touches[i].x < (4*w)-1+ margin){
         fill(0, 255, 0);
-        rect(3*w+ margin, 60, w-1, 200);
+        rect(3*w+ margin, w, w-1, 200);
         F4.freq(pitches[3]);
         F4.amp(val, 0.5);
       }
       if(4*w+ margin < touches[i].x && touches[i].x < (5*w)-1+ margin){
         fill(0, 0, 255);
-        rect(4*w+ margin, 60, w-1, 200);
+        rect(4*w+ margin, w, w-1, 200);
         G4.freq(pitches[4]);
         G4.amp(val, 0.5);
       }
       if(5*w+ margin < touches[i].x && touches[i].x < (6*w)-1+ margin){
         fill(0, 5, 128);
-        rect(5*w+ margin, 60, w-1, 200);
+        rect(5*w+ margin, w, w-1, 200);
         A4.freq(pitches[5]);
         A4.amp(val, 0.5);
       }
       if(6*w + margin< touches[i].x && touches[i].x < (7*w)-1+ margin){
         fill(100, 0, 255);
-        rect(6*w+ margin, 60, w-1, 200);
+        rect(6*w+ margin, w, w-1, 200);
         B4.freq(pitches[6]);
         B4.amp(val, 0.5);
       }
       if(7*w+ margin < touches[i].x && touches[i].x < (8*w)-1+ margin){
         fill(255, 0, 0);
-        rect(7*w+ margin, 60, w-1, 200);
+        rect(7*w+ margin, w, w-1, 200);
         C5.freq(pitches[7]);
         C5.amp(val, 0.5);
       }
       if(8*w+ margin < touches[i].x && touches[i].x < (9*w)-1+ margin){
         fill(255, 50, 0);
-        rect(8*w+ margin, 60, w-1, 200);
+        rect(8*w+ margin, w, w-1, 200);
         D5.freq(pitches[8]);
         D5.amp(val, 0.5);
       }
       if(9*w+ margin < touches[i].x && touches[i].x < (10*w)-1+ margin){
         fill(255, 255, 0);
-        rect(9*w+ margin, 60, w-1, 200);
+        rect(9*w+ margin, w, w-1, 200);
         E5.freq(pitches[9]);
         E5.amp(val, 0.5);
       }
@@ -176,7 +185,7 @@ function touchStarted(){
   getAudioContext().resume();
   
   for(var i = 0; i < touches.length; i++){
-    if(60 < touches[i].y && touches[i].y < 200){
+    if(w < touches[i].y && touches[i].y < 200){
       if(0 + margin < touches[i].x && touches[i].x < w-1+ margin){
         C4.start();
       }
